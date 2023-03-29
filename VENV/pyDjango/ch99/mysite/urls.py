2ed6bf41dd685d99b653e14f39c7850f05819225
young <smon0376@naver.com>
2023-03-29 15:11:31 +0900

    03_29_blog_end

--- VENV/pyDjango/ch99/mysite/urls.py
+++ VENV/pyDjango/ch99/mysite/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-#URLconf 뷰를 호출하므로 뷰 모듈의 관련 클래스를 임포트함
-from bookmark.views import BookmarkLV, BookmarkDV
+from django.urls import path,include
 
-urlpatterns = [
+#URLconf 뷰를 호출하므로 뷰 모듈의 관련 클래스를 임포트함
+#from bookmark.views import BookmarkLV, BookmarkDV
+
+urlpatterns = [    
     path('admin/',admin.site.urls),
-    path('bookmark/',BookmarkLV.as_view(),name='index'),
-    path('bookmark/<int:pk>/',BookmarkDV.as_view(),name='detail'),
+    path('bookmark/', include('bookmark.urls')),
+    path('blog/', include('blog.urls')),
+    #path('bookmark/',BookmarkLV.as_view(),name='index'),
+    #path('bookmark/<int:pk>/',BookmarkDV.as_view(),name='detail'),
+
 ]
