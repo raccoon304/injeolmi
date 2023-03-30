e7c854bfacc44e2e0bba1d9903f166d44b080d92
young <smon0376@naver.com>
2023-03-30 11:21:59 +0900

    03_30_updating

--- VENV/pyDjango/ch99/mysite/urls.py
+++ VENV/pyDjango/ch99/mysite/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
+from mysite.views import HomeView
 
 #URLconf 뷰를 호출하므로 뷰 모듈의 관련 클래스를 임포트함
 #from bookmark.views import BookmarkLV, BookmarkDV
@@ -8,6 +9,7 @@ urlpatterns = [
     path('admin/',admin.site.urls),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
+    path('',HomeView.as_view(), name='home'),
     #path('bookmark/',BookmarkLV.as_view(),name='index'),
     #path('bookmark/<int:pk>/',BookmarkDV.as_view(),name='detail'),
 
